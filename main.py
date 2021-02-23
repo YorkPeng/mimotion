0e90b65506d525bf91af3e883de937dd999ecc81
York <516585610@qq.com>
2021-02-23 15:58:03 +0800

    Update main.py
    去除信息输出

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ def main(user, passwd, step):
     response = requests.post(url, data=data, headers=head).json()
     #print(response)
     result = f"{user[:4]}****{user[-4:]}: [{now}] 修改步数（{step}）"+ response['message']
-    print(result)
+    # print(result)
     return result
   
 #获取时间戳
